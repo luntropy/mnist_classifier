6c6ee86cd01181b005095ef0149eca1cf1571eae
Betina Borisova <b.borisowa17@gmail.com>
2023-03-11 20:51:24 +0200

    Minor configuration changes

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ MAX_BUFFER_SIZE = 524288000
 
 OPTIONS = { 'swagger_path': swagger_ui_3_path, 'swagger_url': '/' }
 
-app = connexion.App(__name__, specification_dir = 'spec/', server = 'tornado', options = OPTIONS)
-# app = connexion.App(__name__, specification_dir = 'spec/', options = OPTIONS)
+#app = connexion.App(__name__, specification_dir = 'spec/', server = 'tornado', options = OPTIONS)
+app = connexion.App(__name__, specification_dir = 'spec/', options = OPTIONS)
 app.add_api('service.yaml')
 
 if __name__ == '__main__':
-    app.run(host = HOST, port = API_PORT, max_buffer_size = MAX_BUFFER_SIZE)
-    # app.run(host = HOST, port = API_PORT)
+    #app.run(host = HOST, port = API_PORT, max_buffer_size = MAX_BUFFER_SIZE)
+    app.run(host = HOST, port = API_PORT)
